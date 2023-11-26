3a9b6af3efe5931d1b13613b1a54304968ef65cf
xuantuong97 <tieutho2026@gmail.com>
2023-11-26 21:12:55 +0700

    l

--- bai tap/bai 7 - Validation/validate_music/src/main/java/com/example/validation/controller/SongController.java
+++ bai tap/bai 7 - Validation/validate_music/src/main/java/com/example/validation/controller/SongController.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/song")
@@ -38,5 +40,22 @@ public class SongController {
         BeanUtils.copyProperties(songDto, song);
         songService.save(song);
         return "redirect:/song";
+    }
+    @GetMapping("/edit")
+    public String showEditForm(Model model, @RequestParam int id){
+        Song song = songService.findById(id);
+        model.addAttribute("song", song);
+        return "/edit";
+    }
+    @PostMapping("/edit")
+    public String editSong(@Valid RedirectAttributes redirectAttributes, Song song, BindingResult bindingResult){
+        SongDto songDto = new SongDto();
+        BeanUtils.copyProperties(song, songDto);
+        new SongDto().validate(songDto,bindingResult);
+        if(bindingResult.hasErrors()){
+            return "/edit";
+        }
+        songService.save(song);
+        return "redirect:/list";
     }
 }
